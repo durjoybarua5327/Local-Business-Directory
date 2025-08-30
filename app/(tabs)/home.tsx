eb5d6b6a075133f0d115dbf86acb9f82216b8feb
Durjoy Barua <142112376+durjoybarua5327@users.noreply.github.com>
2025-08-30 13:10:29 +0600

    commit

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export default function home() {
   return (
     <View>
-      <Text>home</Text>
+      <Text style={{fontSize:60, fontFamily:'Outfit-Extra-Bold'}}>home</Text>
     </View>
   )
 }
